94645a5627f0e0dc3ae41ce94ccf24a438975fed
Angy Ortiz <62076779+oangy@users.noreply.github.com>
2020-03-19 14:54:00 -0500

    preguntas hechas

--- interfaz_web/preguntas.sql
+++ interfaz_web/preguntas.sql
@@ -11,6 +11,6 @@ WHERE(id_empleado in ((SELECT documento FROM empleado)))
 
 GROUP BY documento;
 #---PREGUNTA 3 Numero de contratos que tiene un empleado--------------
-
+SELECT * FROM ((SELECT * FROM(producto INNER JOIN factura_producto ON factura_producto.id_producto LIKE producto.codigo))AS tablita) WHERE(tablita.id_factura IN (SELECT codigo FROM factura WHERE(factura.id_empleado LIKE '1036678350'))) 
 #---PREGUNTA 4 Total de dinero en ventas de productos--------------
 SELECT SUM(precio) AS total FROM (producto INNER JOIN factura_producto ON factura_producto.id_producto LIKE producto.codigo) ;
