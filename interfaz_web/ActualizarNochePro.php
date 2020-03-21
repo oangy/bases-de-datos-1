02fb3d0a9faa93fd6bd869cd8688b66a8c9d1bd5
Angy Ortiz <62076779+oangy@users.noreply.github.com>
2020-03-21 16:14:00 -0500

    finalle

--- interfaz_web/ActualizarNochePro.php
+++ interfaz_web/ActualizarNochePro.php
@@ -5,6 +5,19 @@ extract($_POST);
 
 $query = "UPDATE noche SET nombre='$nombre_n',id_grupo='$id_grupo_n',id_solista='$id_solista_n' WHERE fecha='$fecha_v'";
 
+
+if($id_grupo_n==null){
+    $query = "UPDATE noche SET nombre='$nombre_n',id_solista='$id_solista_n' WHERE fecha='$fecha_v'";
+
+}
+if($id_solista_n==null){
+    $query = "UPDATE noche SET nombre='$nombre_n',id_grupo='$id_grupo_n', WHERE fecha='$fecha_v'";
+
+}
+if($id_grupo_n!=null && $id_solista_n!=null){
+    echo "Error solo se puede haber presentado un artista o un grupo en la noche.";
+}
+
 $result= mysqli_query($conexion,$query);
 
 if($result){
